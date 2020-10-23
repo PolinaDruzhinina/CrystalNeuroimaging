261934dcfd18159004a9b21c373cbc8150526bcb
polina <buzoilic@gmail.com>
2020-10-23 06:12:29 +0300

    add source

--- masks/grad_cam.py
+++ masks/grad_cam.py
@@ -1,49 +1,9 @@
+#Source from https://github.com/maxs-kan/InterpretableNeuroDL
 import torch
 import torch.nn as nn
 import numpy as np
 from tqdm import tqdm
 import torch.nn.functional as F
-class MriNetGrad(nn.Module):
-    def __init__(self, c):
-        super(MriNetGrad, self).__init__()
-        self.features = nn.Sequential( 
-                nn.Conv3d(1, c, kernel_size=3, stride=1, dilation=1, padding=0),
-                nn.BatchNorm3d(c),
-                nn.ReLU(),
-                nn.MaxPool3d(kernel_size=2, stride=2,),
-                
-                nn.Conv3d(c, 2*c, kernel_size=3, stride=1, dilation=1, padding=0),
-                nn.BatchNorm3d(2*c),
-                nn.ReLU(),
-                nn.MaxPool3d(kernel_size=2, stride=2),
-                
-                nn.Conv3d(2*c, 4*c, kernel_size=3, stride=1, dilation=1, padding=0),
-                nn.BatchNorm3d(4*c),
-                nn.ReLU()
-        )
-        self.classifier = nn.Sequential(
-            nn.MaxPool3d(kernel_size=2, stride=2),
-            nn.Flatten(),
-            nn.Dropout(p=0.5),
-            nn.Linear(in_features=4*c*5*7*5, out_features=2),
-        )
-        self.gradients = None
-        
-  
-    def activations_hook(self, grad):
-        self.gradients = grad
-
-    def forward(self, x):
-        x = self.features(x)
-        h = x.register_hook(self.activations_hook)
-        x = self.classifier(x)
-        return x
-    
-    def get_activations_gradient(self):
-        return self.gradients
-    
-    def get_activations(self, x):
-        return self.features(x)
     
 class GuidedBackprop():
     def __init__(self, model):
--- masks/meanpertrub.py
+++ masks/meanpertrub.py
@@ -1,3 +1,4 @@
+#Source from https://github.com/maxs-kan/InterpretableNeuroDL
 import numpy as np
 import torch
 import torch.nn as nn
